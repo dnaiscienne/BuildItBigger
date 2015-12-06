2ee0ccebc7ec6eda01028f69945e09120c0a92bb
dnaiscienne <dnaiscienne@yahoo.com>
2015-12-06 22:43:11 +0800

    Set up connected test to check if joke returned a non-empty string

---
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/FetchJokeTaskTest.java
@@ -0,0 +1,29 @@
+package com.udacity.gradle.builditbigger;
+
+import android.test.AndroidTestCase;
+
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * Created by DS on 12/6/2015.
+ */
+public class FetchJokeTaskTest extends AndroidTestCase implements FetchJokeTask.OnTaskCompleted{
+
+// Reference:
+// http://blog.zadorozhnyi.com/2012/06/19/unit-testing-asynctask-descendants/
+
+    final CountDownLatch signal = new CountDownLatch(1);
+
+    public void testFetchJokeTask()throws InterruptedException{
+        final FetchJokeTask fetchJokeTask = new FetchJokeTask(this);
+        fetchJokeTask.execute(getContext());
+        signal.await();
+    }
+
+    @Override
+    public void onTaskCompleted(String joke) {
+        assertNotNull(joke);
+        assertTrue(joke.length()>0);
+        signal.countDown();
+    }
+}
